5ea23efc9e966bd3a38a881f551ed38d1cee6e13
Choi TaeSan <xotks7524@gmail.com>
2024-04-19 23:39:14 +0900

    너비 우선 탐색 추가

---
+++ src/BFS.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited, queue = set(), deque([start])
+    visited.add(start)
+    while queue:
+        vertex = queue.popleft()
+        print(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# 사용 예
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+bfs(graph, 'A')
